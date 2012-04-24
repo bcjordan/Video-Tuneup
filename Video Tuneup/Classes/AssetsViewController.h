931df7ee7d144ccfe9cbc33e381bf46910d0054c
Teddy Wing <twing@teddywing.com>
2012-04-24 14:25:53 -0400

    Added AssetsViewController to show the video library in a tableview

---
+++ Video Tuneup/Classes/AssetsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AssetsViewController.h
+//  Video Tuneup
+//
+//  Created by tw on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AssetsViewController : UIViewController
+
+@end
