d8ee19b6c2807322227c58019c7677ae73ae8638
Teddy Wing <twing@teddywing.com>
2012-04-25 23:28:21 -0400

    Added WebserviceCommunicator class to send a music file up to the Stewardess webservice and get a file back.
    TODO: replace 'void' with a file or file_path

---
+++ Video Tuneup/Classes/WebserviceCommunicator.h
@@ -0,0 +1,15 @@
+//
+//  WebserviceCommunicator.h
+//  Video Tuneup
+//
+//  Created by tw on 4/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WebserviceCommunicator : NSObject
+
+-(void)mixMusic:(NSURL *)file;
+
+@end
---
+++ Video Tuneup/Classes/WebserviceCommunicator.m
@@ -0,0 +1,17 @@
+//
+//  WebserviceCommunicator.m
+//  Video Tuneup
+//
+//  Created by tw on 4/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "WebserviceCommunicator.h"
+
+@implementation WebserviceCommunicator
+
+-(void)mixMusic:(NSURL *)file {
+    
+}
+
+@end
